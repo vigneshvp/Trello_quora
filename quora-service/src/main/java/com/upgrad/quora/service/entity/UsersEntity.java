2b89c3b52015ac8308996a4c14a49b862483e50f
kaniGupta <kanishkgupta13@gmail.com>
2021-01-03 00:20:29 +0530

    Updated User Entity.

--- quora-service/src/main/java/com/upgrad/quora/service/entity/UsersEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UsersEntity.java
@@ -20,10 +20,13 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "users")
-@NamedQueries({
-                      @NamedQuery(name = "usersByUuid",
-                                  query = "select u from UsersEntity u where u.uuid = :uuid")
-              })
+@NamedQueries(
+        {
+                @NamedQuery(name = "userByUuid", query = "select u from UsersEntity u where u.uuid = :uuid"),
+                @NamedQuery(name = "userByEmail", query = "select u from UsersEntity u where u.email =:email"),
+                @NamedQuery(name = "userByUserName", query = "select u from UsersEntity u where u.username =:username")
+        }
+)
 public class UsersEntity implements Serializable {
     private static final long serialVersionUID = 4002003379453838315L;
 
