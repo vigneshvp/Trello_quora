66ae9d592bd50042b104370c1724eb3fca2e0e31
vigneshvp <viki3110@gmail.com>
2021-01-02 16:00:18 +0530

    Created Service Layer for Signup
    Added service classes for Signup functionality.

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/SignupController.java
@@ -0,0 +1,49 @@
+package com.upgrad.quora.api.controller;
+
+
+import com.upgrad.quora.api.model.SignupUserRequest;
+import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.SignupBusinessService;
+import com.upgrad.quora.service.entity.UsersEntity;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class SignupController {
+    
+    @Autowired
+    private SignupBusinessService signupBusinessService;
+    
+    @RequestMapping(method = RequestMethod.POST, path = "/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) {
+        
+        final UsersEntity userEntity = new UsersEntity();
+        
+        userEntity.setUuid(UUID.randomUUID().toString());
+        userEntity.setFirstname(signupUserRequest.getFirstName());
+        userEntity.setLastname(signupUserRequest.getLastName());
+        userEntity.setEmail(signupUserRequest.getEmailAddress());
+        userEntity.setPassword(signupUserRequest.getPassword());
+        userEntity.setSalt("1234abc");
+        userEntity.setUsername(signupUserRequest.getUserName());
+        userEntity.setContactnumber(signupUserRequest.getContactNumber());
+        userEntity.setCountry(signupUserRequest.getCountry());
+        userEntity.setAboutme(signupUserRequest.getAboutMe());
+        userEntity.setDob(signupUserRequest.getDob());
+        userEntity.setRole("nonadmin");
+        
+        
+        final UsersEntity createdUserEntity = signupBusinessService.signup(userEntity);
+        SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("REGISTERED");
+        return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
+    }
+    
+}
---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserAdminBusinessService.java
@@ -0,0 +1,33 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UsersEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserAdminBusinessService {
+    
+    @Autowired
+    private UserDao userDao;
+    
+    @Autowired
+    private PasswordCryptographyProvider cryptographyProvider;
+    
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UsersEntity createUser(final UsersEntity userEntity) {
+        
+        String password = userEntity.getPassword();
+        if (password == null) {
+            userEntity.setPassword("proman@123");
+        }
+        String[] encryptedText = cryptographyProvider.encrypt(userEntity.getPassword());
+        userEntity.setSalt(encryptedText[0]);
+        userEntity.setPassword(encryptedText[1]);
+        return userDao.createUser(userEntity);
+        
+    }
+
+}
