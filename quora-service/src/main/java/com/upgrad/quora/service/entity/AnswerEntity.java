6825f9d4be9dbf6aab49bfddf5030ecc2f0293cc
kaniGupta <kanishkgupta13@gmail.com>
2021-01-02 12:44:48 +0530

    Added entity classes

---
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/AnswerEntity.java
@@ -0,0 +1,115 @@
+package com.upgrad.quora.service.entity;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "answer")
+public class AnswerEntity implements Serializable {
+    private static final long serialVersionUID = 8012246708977575749L;
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @NotNull
+    @Size(max = 200)
+    private String uuid;
+
+    @Column(name = "ANS")
+    @NotNull
+    @Size(max = 255)
+    private String ans;
+
+    @Column(name = "DATE")
+    @NotNull
+    private ZonedDateTime date;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private UsersEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "QUESTION_ID")
+    private QuestionEntity question;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(final String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getAns() {
+        return ans;
+    }
+
+    public void setAns(final String ans) {
+        this.ans = ans;
+    }
+
+    public ZonedDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(final ZonedDateTime date) {
+        this.date = date;
+    }
+
+    public UsersEntity getUser() {
+        return user;
+    }
+
+    public void setUser(final UsersEntity user) {
+        this.user = user;
+    }
+
+    public QuestionEntity getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(final QuestionEntity question) {
+        this.question = question;
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
---
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UsersEntity.java
@@ -0,0 +1,191 @@
+package com.upgrad.quora.service.entity;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class UsersEntity implements Serializable {
+    private static final long serialVersionUID = 4002003379453838315L;
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @NotNull
+    @Size(max = 200)
+    private String uuid;
+
+    @Column(name = "FIRSTNAME")
+    @NotNull
+    @Size(max = 30)
+    private String firstname;
+
+    @Column(name = "LASTNAME")
+    @NotNull
+    @Size(max = 30)
+    private String lastname;
+
+    @Column(name = "USERNAME", unique = true)
+    @NotNull
+    @Size(max = 30)
+    private String username;
+
+    @Column(name = "EMAIL", unique = true)
+    @NotNull
+    @Email
+    @Size(max = 50)
+    private String email;
+
+    @Column(name = "PASSWORD")
+    @NotNull
+    @Size(max = 255)
+    private String password;
+
+    @Column(name = "SALT")
+    @NotNull
+    @Size(max = 200)
+    private String salt;
+
+    @Column(name = "COUNTRY")
+    @Size(max = 30)
+    private String country;
+
+    @Column(name = "ABOUTME")
+    @Size(max = 50)
+    private String aboutme;
+
+    @Column(name = "DOB")
+    @Size(max = 30)
+    private String dob;
+
+    @Column(name = "CONTACTNUMBER")
+    @Size(max = 30)
+    private String contactnumber;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(final String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(final String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(final String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(final String salt) {
+        this.salt = salt;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(final String country) {
+        this.country = country;
+    }
+
+    public String getAboutme() {
+        return aboutme;
+    }
+
+    public void setAboutme(final String aboutme) {
+        this.aboutme = aboutme;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(final String dob) {
+        this.dob = dob;
+    }
+
+    public String getContactnumber() {
+        return contactnumber;
+    }
+
+    public void setContactnumber(final String contactnumber) {
+        this.contactnumber = contactnumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
