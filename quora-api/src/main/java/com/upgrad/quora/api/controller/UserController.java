487222655ab9466ee681755e6e73a3f408e47ed8
vigneshvp <viki3110@gmail.com>
2021-01-02 19:50:46 +0530

    Created Signin functionality
    Added service and controller changes to implement signin.
    Changed context root to /user

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,27 +1,33 @@
 package com.upgrad.quora.api.controller;
 
 
+import com.upgrad.quora.api.model.SigninResponse;
 import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
 import com.upgrad.quora.service.business.UserService;
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UsersEntity;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import java.util.Base64;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user")
 @Api(value = "Signup")
 public class UserController {
     
@@ -30,7 +36,7 @@ public class UserController {
     
     @ApiOperation(value = "Signup a user to the quora trello application", response = ResponseEntity.class)
     @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "USER SUCCESSFULLY REGISTERED"),
+        @ApiResponse(code = 201, message = "USER SUCCESSFULLY REGISTERED"),
         @ApiResponse(code = 409, message = "CONFLICT")
     })
     @RequestMapping(method = RequestMethod.POST, path = "/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -55,6 +61,30 @@ public class UserController {
         final UsersEntity createdUserEntity = userService.createUser(userEntity);
         SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
         return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
+    }
+    
+    @ApiOperation(value = "Signin to the quora trello application", response = ResponseEntity.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "USER SUCCESSFULLY REGISTERED"),
+        @ApiResponse(code = 404, message = "RESOURCE NOT FOUND")
+    })
+    @RequestMapping(method = RequestMethod.POST, path = "/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SigninResponse> login(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
+        
+        
+        byte[] decode = Base64.getDecoder().decode(authorization.split("Basic ")[1]);
+        String decodedText = new String(decode);
+        String[] decodedArray = decodedText.split(":");
+        
+        UserAuthEntity userAuthToken = userService.authenticate(decodedArray[0], decodedArray[1]);
+        UsersEntity user = userAuthToken.getUser();
+        
+        SigninResponse signinResponse = new SigninResponse().id(user.getUuid())
+                                                            .message("SIGNED IN SUCCESSFULL");
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("access-token", userAuthToken.getAccessToken());
+        return new ResponseEntity<SigninResponse>(signinResponse, headers, HttpStatus.OK);
     }
     
 }
--- quora-service/src/main/java/com/upgrad/quora/service/business/UserService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserService.java
@@ -1,8 +1,11 @@
 package com.upgrad.quora.service.business;
 
 import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UsersEntity;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
+import java.time.ZonedDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -18,25 +21,52 @@ public class UserService {
     private PasswordCryptographyProvider cryptographyProvider;
     
     @Transactional(propagation = Propagation.REQUIRED)
-    public UsersEntity createUser(final UsersEntity userEntity) throws SignUpRestrictedException  {
+    public UsersEntity createUser(final UsersEntity UsersEntity) throws SignUpRestrictedException  {
         
-        String password = userEntity.getPassword();
-        UsersEntity existingUserEntity;
-        existingUserEntity = userDao.getUserByUserName(userEntity.getUsername());
-        if(existingUserEntity != null) {
+        String password = UsersEntity.getPassword();
+        UsersEntity existingUsersEntity;
+        existingUsersEntity = userDao.getUserByUserName(UsersEntity.getUsername());
+        if(existingUsersEntity != null) {
             throw new SignUpRestrictedException("SGR-001","Try any other Username, this Username has already been taken");
         }
-        existingUserEntity = userDao.getUserByEmail(userEntity.getEmail());
-        if(existingUserEntity != null) {
+        existingUsersEntity = userDao.getUserByEmail(UsersEntity.getEmail());
+        if(existingUsersEntity != null) {
             throw new SignUpRestrictedException("SGR-002","This user has already been registered, try with any other emailId");
         }
         if (password == null) {
-            userEntity.setPassword("quora");
+            UsersEntity.setPassword("quora");
         }
-        String[] encryptedText = cryptographyProvider.encrypt(userEntity.getPassword());
-        userEntity.setSalt(encryptedText[0]);
-        userEntity.setPassword(encryptedText[1]);
-        return userDao.createUser(userEntity);
+        String[] encryptedText = cryptographyProvider.encrypt(UsersEntity.getPassword());
+        UsersEntity.setSalt(encryptedText[0]);
+        UsersEntity.setPassword(encryptedText[1]);
+        return userDao.createUser(UsersEntity);
+        
+    }
+    
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserAuthEntity authenticate(final String username, final String password) throws AuthenticationFailedException {
+        UsersEntity userEntity = userDao.getUserByUserName(username);
+        if (userEntity == null) {
+            throw new AuthenticationFailedException("ATH-001", "This username does not exist");
+        }
+        
+        final String encryptedPassword = cryptographyProvider.encrypt(password, userEntity.getSalt());
+        if (encryptedPassword.equals(userEntity.getPassword())) {
+            JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(encryptedPassword);
+            UserAuthEntity userAuthToken = new UserAuthEntity();
+            userAuthToken.setUser(userEntity);
+            final ZonedDateTime now = ZonedDateTime.now();
+            final ZonedDateTime expiresAt = now.plusHours(8);
+            userAuthToken.setAccessToken(jwtTokenProvider.generateToken(userEntity.getUuid(), now, expiresAt));
+            userAuthToken.setLoginAt(now);
+            userAuthToken.setExpiresAt(expiresAt);
+            userAuthToken.setUuid(userEntity.getUuid());
+            userDao.createAuthToken(userAuthToken);
+            userDao.updateUser(userEntity);
+            return userAuthToken;
+        } else {
+            throw new AuthenticationFailedException("ATH-002", "Password Failed");
+        }
         
     }
 
