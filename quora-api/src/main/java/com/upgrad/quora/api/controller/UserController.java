49af8791f64b80f895ce53617ebad8dea23b0e59
vigneshvp <viki3110@gmail.com>
2021-01-04 06:31:25 +0530

    Changed names of signin and signout methods

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -11,10 +11,6 @@ import com.upgrad.quora.service.entity.UsersEntity;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import com.upgrad.quora.service.exception.SignOutRestrictedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import java.util.Base64;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,19 +26,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-@Api(value = "Signup")
 public class UserController {
     
     @Autowired
     private UserBusinessService userBusinessService;
     
-    @ApiOperation(value = "Signup a user to the quora trello application", response = ResponseEntity.class)
-    @ApiResponses(value = {
-        @ApiResponse(code = 201, message = "USER SUCCESSFULLY REGISTERED"),
-        @ApiResponse(code = 409, message = "CONFLICT")
-    })
+    
     @RequestMapping(method = RequestMethod.POST, path = "/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) throws SignUpRestrictedException {
+    public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest)
+        throws SignUpRestrictedException {
         
         final UsersEntity userEntity = new UsersEntity();
         
@@ -59,20 +51,17 @@ public class UserController {
         userEntity.setDob(signupUserRequest.getDob());
         userEntity.setRole("nonadmin");
         
-        
         final UsersEntity createdUserEntity = userBusinessService.createUser(userEntity);
-        SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("USER SUCCESSFULLY REGISTERED");
+        SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid())
+                                              .status("USER SUCCESSFULLY REGISTERED");
         return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
     }
     
-    @ApiOperation(value = "Signin to the quora trello application", response = ResponseEntity.class)
-    @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "USER SUCCESSFULLY REGISTERED"),
-        @ApiResponse(code = 404, message = "RESOURCE NOT FOUND")
-    })
+    
     @RequestMapping(method = RequestMethod.POST, path = "/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<SigninResponse> login(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
-        
+    public ResponseEntity<SigninResponse> signin(
+        @RequestHeader("authorization") final String authorization)
+        throws AuthenticationFailedException {
         
         byte[] decode = Base64.getDecoder().decode(authorization.split("Basic ")[1]);
         String decodedText = new String(decode);
@@ -83,7 +72,7 @@ public class UserController {
         UsersEntity user = userAuthToken.getUser();
         
         SigninResponse signinResponse = new SigninResponse().id(user.getUuid())
-                                                            .message("SIGNED IN SUCCESSFULL");
+                                            .message("SIGNED IN SUCCESSFULLY");
         
         HttpHeaders headers = new HttpHeaders();
         headers.add("access-token", userAuthToken.getAccessToken());
@@ -91,17 +80,14 @@ public class UserController {
     }
     
     
-    @ApiOperation(value = "Sign-off from the quora trello application", response = ResponseEntity.class)
-    @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "SIGNED OUT SUCCESSFULLY"),
-        @ApiResponse(code = 404, message = "RESOURCE NOT FOUND")
-    })
     @RequestMapping(method = RequestMethod.POST, path = "/signout", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<SignoutResponse> logoff(@RequestHeader("authorization") final String authorization) throws SignOutRestrictedException {
+    public ResponseEntity<SignoutResponse> signout(
+        @RequestHeader("authorization") final String authorization)
+        throws SignOutRestrictedException {
         UserAuthEntity userAuthToken = userBusinessService.logout(authorization);
         UsersEntity user = userAuthToken.getUser();
         SignoutResponse signoutResponse = new SignoutResponse().id(user.getUuid())
-                                            .message("SIGNED OUT SUCCESSFULLY");
+                                              .message("SIGNED OUT SUCCESSFULLY");
         return new ResponseEntity<SignoutResponse>(signoutResponse, HttpStatus.OK);
     }
 }
